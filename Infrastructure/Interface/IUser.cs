2029ec0946cba0ce1fa2ea8d3551e73b2c1bd2f5
waqarahmed034920@gmail.com <waqarahmed034920@gmail.com>
2020-11-30 00:36:59 +0500

    added user repository and model

---
+++ Infrastructure/Interface/IUser.cs
@@ -0,0 +1,17 @@
+﻿using SurveyPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyPortal.Infrastructure.Interface
+{
+    interface IUser : IRepository<User>
+    {
+        User Login(string userid, string password);
+        bool Register(User objUser);
+        bool UpdateProfile(User objUser);
+        bool ChangePassword(string UserId, string Password);
+    }
+}
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using SurveyPortal.Infrastructure.Interface;
+using SurveyPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyPortal.Infrastructure.Repositories
+{
+    public class UserRepository : IUser
+    {
+        public bool ChangePassword(string UserId, string Password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(User objT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Login(string userid, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Register(User objUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(User objT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProfile(User objUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
